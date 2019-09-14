83ac71bae33aaeefab4fd8fc5123ef32ed4972fb
Justin Gatlin <justin@vikingstamp.com>
2019-09-14 12:23:56 -0400

    Make port dynamic

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 import helmet from "helmet";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 import StorageRouter from "StorageRouter";
 
