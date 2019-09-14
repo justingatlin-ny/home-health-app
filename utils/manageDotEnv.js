8ed21c077e2335a3ba5a4c2ec3a3b9692d319ba1
Justin Gatlin <justin@vikingstamp.com>
2019-09-14 11:53:47 -0400

    Updated error logic

--- utils/manageDotEnv.js
+++ utils/manageDotEnv.js
@@ -4,14 +4,19 @@ const path = require("path");
 const dotEnvPath = path.resolve(__dirname, '../', '.env');
 
 const dotEnvExists = fs.existsSync(dotEnvPath);
-const dotEnvFile = dotEnvExists && fs.readFileSync(dotEnvPath, 'utf8') || '';
+if (dotEnvExists) {
+    const dotEnvFile = fs.readFileSync(dotEnvPath, 'utf8') || '';
 
-const lineRE = /\r?\n/;
-const dotEnvLinesArr = dotEnvFile.split(lineRE);
+    const lineRE = /\r?\n/;
+    const dotEnvLinesArr = dotEnvFile.split(lineRE);
+    
+    dotEnvLinesArr.forEach(line => {
+      const lineArr = line.split('=');
+      const key = lineArr[0];
+      const value = lineArr[1];
+      process.env[key] = value;
+    });
+} else {
+    console.error(`No .env file exists here or could not be read: ${dotEnvPath}`)
+}
 
-dotEnvLinesArr.forEach(line => {
-  const lineArr = line.split('=');
-  const key = lineArr[0];
-  const value = lineArr[1];
-  process.env[key] = value;
-});
